bd485a1d947e5b46333d811505440c6905e113cb
ArkhipovAS <arhipov.sasha@gmail.com>
2018-12-12 12:50:17 +0300

    add lesson6

--- src/com/arkhipov/hw6/Lesson6.java
+++ src/com/arkhipov/hw6/Lesson6.java
@@ -1,4 +1,16 @@
 package com.arkhipov.hw6;
 
 public class Lesson6 {
+    public static void main(String[] args) {
+        BST<Character, Integer> assArr = new BST<>();
+        assArr.put('S', 123);
+        assArr.put('E', 345);
+        assArr.put('A', 123);
+        assArr.put('C', 987);
+        assArr.put('R', 911);
+        assArr.put('H', 112);
+        assArr.put('M', 012);
+        assArr.put('X', 003);
+        System.out.println();
+    }
 }
